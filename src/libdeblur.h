b24f964df1d4e4d16a96918765f47a3332277334
Suzuki Hironobu <hironobu@interdb.jp>
2016-10-10 01:59:20 +0900

    changed libdeblur.h

--- src/libdeblur.h
+++ src/libdeblur.h
@@ -26,9 +26,9 @@ using namespace Eigen;
  -------------------------------------------------------*/
 enum
   {
-    DEBLUR_HIGH,
+    DEBLUR_LOW,
     DEBLUR_MEDIUM,
-    DEBLUR_LOW
+    DEBLUR_HIGH
   };
 
 /*-------------------------------------------------------
@@ -43,25 +43,25 @@ typedef struct vrange
 typedef struct param
 {
   int mode;
-  int base_8c9afa73d5fb2e5a964ee212c8ae11eb;
-  int base_1b7de9732286185efd1dbce52a1f29ba;
-  int base_9d2e966f4abf99089fa91c874cbbc4aa;
-  int base_c8bb87c35d5c8016039eb2c63f5055a1;
-  int base_c8672b53175c78bd40dfa26a7aae49ec;
-  double base_05b048d7242cb7b8b57cfa3b1d65ecea;
-  double base_83aa590baf7250511fba793140bd1ea0;
-  double base_2ec348ba37e0003b20442504ceb32648;
-  double base_3643e51d870f4849a9dfe990c779ad9d;
-  double base_48614ae9d92989d76e508df68f7399b6;
-  double base_8c4128e380caf5f0e88a870e04e8c397;
-  double base_5f17275558315808e826bd4d916f9917;
-  double base_90c0215603f7090fd20ba9e4cfecb70f;
-  double base_085c6920b37fd444ee73a0ebe8e929c6;
-  int base_331cba6400bc264dc96dd7549b144eb7;
-  int base_91155c5fcc54f25919f9ac1461ca7299;
-  int base_c1c7c2b22243f2eaea86834e3d7bb451;
-  int base_4508a9419f4e73e8274f03db1a5b333f;
-  double base_8baf4edfac39cf03451a836f1fd0a079;
+  int psf_size_x;
+  int psf_size_y;
+  int maxROIsize_r;
+  int maxROIsize_c;
+  int MSlevels;
+  double gamma;
+  double Lp;
+  double beta_h;
+  double alpha_h;
+  double centering_threshold;
+  double beta_u;
+  double alpha_u;
+  double gamma_nonblind;
+  double beta_u_nonblind;
+  int Lp_nonblind;
+  int maxiter_u;
+  int maxiter_h;
+  int maxiter;
+  double ccreltol;
   double filter;
 
 } param_t;
@@ -150,53 +150,53 @@ private:
 
   int sfix(const double);
   double sumabs2 (const MatrixXcd &);
-  void base_1872614a65a70d51910ee7ced46d9bf5 (const MatrixXd &, MatrixXd &, const int, const int);
-  void base_1872614a65a70d51910ee7ced46d9bf5 (const MatrixXd &, MatrixXd &, const float);
-  void base_1872614a65a70d51910ee7ced46d9bf5 (const MatrixXd &, MatrixXd &);
-  void base_5b18cd4b4b96f7299e4097fda90288e3 (MatrixXd &, double &, double &);
-  void base_2fea1f166d05e7dde8763061bb16e801 (MatrixXd &, const MatrixXd &, const MatrixXd &, const vu_star_t);
-  void base_85745d479dd5e1cd108b054d8a48620b (const double, double, double, vu_star_t *);
-  void base_7cccd49cb2e59a1d0632bd4befb14c67 (const MatrixXd &, int &, int &, int &, int &, const double);
-  void base_edfcc099f60103785bd7668cdbb49cca (const MatrixXd &, MatrixXd &, const double);
-  void base_8a42df5b19795bad5870e5a2cacec755 (MatrixXd &);
-  void base_cbb2ace6efa94a7fe34ca9ee1b542e53 (MatrixXd &);
-  void base_6667ce9bd69d8446067e41f669194952 (MatrixXd &, const int, const int);
-  void base_6de88db41628e1fd44e929ffcb14070f (MatrixXd &, const param_t);
+  void imresize (const MatrixXd &, MatrixXd &, const int, const int);
+  void imresize (const MatrixXd &, MatrixXd &, const float);
+  void imresize (const MatrixXd &, MatrixXd &);
+  void normalizeImage (MatrixXd &, double &, double &);
+  void aLn (MatrixXd &, const MatrixXd &, const MatrixXd &, const vu_star_t);
+  void asetLnorm (const double, double, double, vu_star_t *);
+  void get_margins_mat (const MatrixXd &, int &, int &, int &, int &, const double);
+  void make_mask_mat (const MatrixXd &, MatrixXd &, const double);
+  void mat2gray (MatrixXd &);
+  void bwmorph_clean (MatrixXd &);
+  void set_Vh (MatrixXd &, const int, const int);
+  void centerPSF (MatrixXd &, const param_t);
 
-  void PSFestimbase_2fea1f166d05e7dde8763061bb16e801oRgrad (MatrixXd &, MatrixXcd &, param_t, const int);
-  void base_2079a4b8e2329f166ff90bc9604b1956 (const double, MatrixXd &, MatrixXd &, MatrixXcd &, MatrixXcd &, MatrixXcd &, MatrixXcd &,
+  void PSFestimaLnoRgrad (MatrixXd &, MatrixXcd &, param_t, const int);
+  void Ustep (const double, MatrixXd &, MatrixXd &, MatrixXcd &, MatrixXcd &, MatrixXcd &, MatrixXcd &,
 	      MatrixXcd &, MatrixXcd &, MatrixXd &, MatrixXd &, MatrixXd &, MatrixXd &, MatrixXcd &,
 	      MatrixXcd &, MatrixXcd &, MatrixXcd &, MatrixXcd &, MatrixXcd &, MatrixXd &, MatrixXd &,
 	      MatrixXd &, MatrixXd &, MatrixXd &, MatrixXcd &, MatrixXcd &, MatrixXd &, MatrixXd &,
 	      const int, const int, param_t, double, const int, const int);
-  void base_4e59fbcfff3e3dd649cdf148726bffcb (double, MatrixXd &, MatrixXcd &, MatrixXcd &, MatrixXcd &, MatrixXcd &,
+  void Hstep (double, MatrixXd &, MatrixXcd &, MatrixXcd &, MatrixXcd &, MatrixXcd &,
 	      MatrixXd &, MatrixXd &, MatrixXcd &, MatrixXd &, MatrixXd &, MatrixXd &,
 	      MatrixXcd &, MatrixXcd &, MatrixXcd &, MatrixXcd &, MatrixXcd &, MatrixXcd &,
 	      MatrixXd &, const int, const int, const int, const int, param_t, double, const int, const int);
-  void base_3c005cc3ace054db49702f3003191eee (const MatrixXd &, vrange_t &);
-  void base_00adb5c87e5ac2ab7ac7c442d1ccc95a (MatrixXcd &, const vrange_t);
-  void base_930ca8b114a91ebda3e9e9697d00cb48 (MatrixXd &, MatrixXd &, MatrixXcd &, const param_t);
-  MatrixXd base_c6dbff44d02a742559d452259555580c (const MatrixXd &, const param_t);
-  void base_a813d46a372ef1707696d5afa5938ac8(MatrixXd &, const int, const param_t);
-  void base_dbd3d60ff27d1ece1c15f497380749f2(MatrixXd &);
-  void base_d16862fd866aec72be2ef18eba71e2a0(Mat, MatrixXd &, MatrixXd &, MatrixXd &,
+  void set_vrange (const MatrixXd &, vrange_t &);
+  void uConstr (MatrixXcd &, const vrange_t);
+  void fftCGSRaL (MatrixXd &, MatrixXd &, MatrixXcd &, const param_t);
+  MatrixXd getROI (const MatrixXd &, const param_t);
+  void createROI(MatrixXd &, const int, const param_t);
+  void doublePSF(MatrixXd &);
+  void divide_rgb_images(Mat, MatrixXd &, MatrixXd &, MatrixXd &,
 			 double &, double &, double &, double &, double &, double &);
-  void base_a615dbaa3cce1eb9f55ff8fd4c312019(Mat &, const MatrixXcd, const MatrixXcd, const MatrixXcd, const double, 
+  void make_image(Mat &, const MatrixXcd, const MatrixXcd, const MatrixXcd, const double, 
 		  const double, const double, const double, const double, const double);
-  void base_2dd381f8cfd647e52417b13c61c06e7a(const MatrixXd &);
-  void base_1b77cbdb44bc9c713e38d4a055e1247a(MatrixXd &);
-  void base_bc63da0ff03c844bf2b48f01a9e14aa5(Mat &, const param_t);
-  void base_841040e67396f60862af8530508993a8 (param_t *);
-  void base_841040e67396f60862af8530508993a8 ();  
+  void show_psf(const MatrixXd &);
+  void matNormalize(MatrixXd &);
+  void sharp_filter(Mat &, const param_t);
+  void init_param (param_t *);
+  void init_param ();  
 
   // workload
-  int base_cc7bcf5e6a4856507f072bee6f077389;
-  int base_7edabf994b76a00cbc60c95af337db8f;
-  int base_886bb73b3156b0aa24aac99d2de0b238;
-  void base_ce2698d4f64659528f0613662206f59d(param_t *);
+  int totalWorkload;
+  int weight;
+  int counter;
+  void calcTotalWorkload(param_t *);
 
-  void base_dbd14b23cdaf2dc59b90da4493314837(void);
-  void base_7ba3b0eea66eec11b838f65475363d1b(const int);
+  void countUp(void);
+  void changeWeight(const int);
 };
 
 #endif
